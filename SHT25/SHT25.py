da69daaa7b33145e6fae4906b112f24561073269
Jakub Kákona <kaklik@kaklik-netbook.(none)>
2013-11-12 18:15:30 +0100

    repair of syntax

--- SHT25/SHT25.py
+++ SHT25/SHT25.py
@@ -6,25 +6,27 @@
 import smbus
 import time
 
+class SHT25:
 
-SHT25_HEATER_ON = 0x04
-SHT25_HEATER_OFF = 0x00
-SHT25_OTP_reload_off = 0x02
-SHT25_RH12_T14 = 0x00
-SHT25_RH8_T12 = 0x01
-SHT25_RH10_T13 = 0x80
-SHT25_RH11_T11 = 0x81
+  SHT25_HEATER_ON = 0x04
+  SHT25_HEATER_OFF = 0x00
+  SHT25_OTP_reload_off = 0x02
+  SHT25_RH12_T14 = 0x00 
+  SHT25_RH8_T12 = 0x01
+  SHT25_RH10_T13 = 0x80
+  SHT25_RH11_T11 = 0x81
 
-SHT25_ADDR = 0x80
+  SHT25_ADDR = 0x80
 
+bus_number =5
 
 def soft_reset():
-  bus = smbus.SMBus(I2CHUB_bus_number);
+  bus = smbus.SMBus(bus_number);
   bus.write_byte(SHT25_ADDR, 0xFE);
   return
 
 def setup( setup_reg ):  # writes to status register and returns its value
-  bus = smbus.SMBus(I2CHUB_bus_number);
+  bus = smbus.SMBus(bus_number);
   reg=bus.read_byte(SHT25_ADDR, 0xE7);    # Read status actual status register
   reg = (reg & 0x3A) | setup_reg;    # modify actual register status
   bus.i2c_write_bloks(SHT25_ADDR, (0xE6, reg) ); # write new status register
@@ -32,7 +34,7 @@ def setup( setup_reg ):  # writes to status register and returns its value
   return (reg);
 
 def get_temp():
-   bus = smbus.SMBus(I2CHUB_bus_number);
+   bus = smbus.SMBus(bus_number);
    bus.write_byte(SHT25_ADDR, 0xE3); # start temperature measurement
    time.sleep(0.1)
    
@@ -40,14 +42,14 @@ def get_temp():
    LSB=bus.read_byte(SHT25_ADDR)
    Check=bus.read_byte(SHT25_ADDR)
       
-   LSB = LSB >> 2; // trow out status bits
+   LSB = LSB >> 2; # trow out status bits
 
-   data = (((unsigned int16) MSB << 8) + (LSB << 4));
-   return(-46.85 + 175.72*((float)data/0xFFFF));
+   data = (( MSB << 8) + (LSB << 4));
+   return(-46.85 + 175.72*(float(data)/0xFFFF));
 
 
 def get_hum():
-   bus = smbus.SMBus(I2CHUB_bus_number);
+   bus = smbus.SMBus(bus_number);
    bus.write_byte(SHT25_ADDR, 0xE5); # start humidity measurement
    time.sleep(0.1)
 
@@ -55,10 +57,10 @@ def get_hum():
    LSB=bus.read_byte(SHT25_ADDR)
    Check=bus.read_byte(SHT25_ADDR)
 
-   LSB = LSB >> 2; // trow out status bits
+   LSB = LSB >> 2; # trow out status bits
 
-   data = (((unsigned int16) MSB << 8) + (LSB << 4) );
-   return( -6.0 + 125.0*((float)data/0xFFFF));
+   data = ((MSB << 8) + (LSB << 4) );
+   return( -6.0 + 125.0*(float(data) /  0xFFFF));
 
 
 
--- SHT25/SHT25_Example.py
+++ SHT25/SHT25_Example.py
@@ -3,25 +3,25 @@
 import SHT25
 import time
 
-   SHT25.soft_reset();
-   print "SHT25 humidity and temperature sensor example \r\n"
-   print "Temperature  Humidity[%%]  \r\n"
-   time.sleep(0.5)
+SHT25.soft_reset();
+print "SHT25 humidity and temperature sensor example \r\n"
+print "Temperature  Humidity[%%]  \r\n"
+time.sleep(0.5)
 
-  while True:
+while True:
 
-    if i<100: 
-      sht_config = SHT25_RH12_T14 | SHT25_HEATER_OFF; # loop alters on chip heater on and off to check correct function
-    else:
-      sht_config = SHT25_RH12_T14 | SHT25_HEATER_ON;
-    if i > 120: 
-      i = 0;
+  if i<100: 
+    sht_config = SHT25_RH12_T14 | SHT25_HEATER_OFF; # loop alters on chip heater on and off to check correct function
+  else:
+    sht_config = SHT25_RH12_T14 | SHT25_HEATER_ON;
+  if i > 120: 
+    i = 0;
    
-    temperature = SHT25.get_temp();
-    humidity = SHT2.get_hum();
+  temperature = SHT25.get_temp();
+  humidity = SHT2.get_hum();
 
-    print temperature, humidity, SHT25_setup(sht_config), sht_config
-    i++;
-    time.sleep(1)
+  print temperature, humidity, SHT25_setup(sht_config), sht_config
+  i=i+1
+  time.sleep(1)
 
 
