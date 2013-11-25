023f6478d176d204a0a3e89ea118d893dcc1e8f9
Jan Milik <milikjan@fit.cvut.cz>
2013-11-25 18:53:31 +0100

    Fix indents in pymlab.tests.sensors.config and add import check.

--- src/pymlab/tests/sensors/config.py
+++ src/pymlab/tests/sensors/config.py
@@ -9,44 +9,48 @@ Author: Jan Milík <milikjan@fit.cvut.cz>
 import unittest
 import cStringIO as StringIO
 
-from pymlab import config
+try:
+    from pymlab import config
+except ImportError:
+    config = None
 
 
+@unittest.skipIf(config is None, "Could not import `pymlab.config`.")
 class ConfigTestCase(unittest.TestCase):
-	def test_load_python_00(self):
-		cfg = config.Config()
-		cfg.load_python("""
+    def test_load_python_00(self):
+        cfg = config.Config()
+        cfg.load_python("""
 port = 5
 
 bus = [
 ]
-		""")
+        """)
 
-	def test_load_python_01(self):
-		cfg = config.Config()
-		cfg.load_python("""
+    def test_load_python_01(self):
+        cfg = config.Config()
+        cfg.load_python("""
 port = 5
 
 bus = [
-	{ "type": "mag01", "address": 0x68 },
-	{ "type": "sht25" }
+    { "type": "mag01", "address": 0x68 },
+    { "type": "sht25" }
 ]
-		""")
+        """)
 
-	def test_load_python_02(self):
-		cfg = config.Config()
-		cfg.load_python("""
+    def test_load_python_02(self):
+        cfg = config.Config()
+        cfg.load_python("""
 port = 5
 
 bus = [
-	{ "type": "i2chub", "address": 0x70, "children": [ {"type": "mag01", "channel": 0}, {"type": "mag01", "channel": 1}, ] },
-	{ "type": "mag01", "address": 0x68 }
+    { "type": "i2chub", "address": 0x70, "children": [ {"type": "mag01", "channel": 0}, {"type": "mag01", "channel": 1}, ] },
+    { "type": "mag01", "address": 0x68 }
 ]
-		""")
-
-	def test_load_python_03(self):
-		cfg = config.Config()
-		cfg.load_python("""
+        """)
+    
+    def test_load_python_03(self):
+        cfg = config.Config()
+        cfg.load_python("""
 port = 1
 
 bus = [
@@ -69,7 +73,7 @@ bus = [
     },
 ]
 
-		""")
+        """)
 
 
 def main():
