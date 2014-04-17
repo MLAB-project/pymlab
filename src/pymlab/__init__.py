b1a9997e028b4a3deec398cdbf108ceaddb65999
Jakub Kákona <kaklik@mlab.cz>
2014-04-18 00:48:41 +0200

    Description was improved.

--- src/pymlab/__init__.py
+++ src/pymlab/__init__.py
@@ -3,6 +3,25 @@
 """pymlab module.
 
 MLAB I2C bus modules binding.
+
+Currently supported sensors are:
+
+- ALTIMET01A (MPL3115A2)
+- LTS01A    (MAX31725)
+- MAG01A    (HMC5883L)
+- SHT25v01A (Sensirion SHT25)
+
+Currently supported devices are:
+
+- ACOUNTER02A
+- CLKGEN01B   (Silicon Labs Si570 oscillator)
+- I2CHUB02A (TCA9548A)
+
+Currently supported interfaces are:
+
+- Kernel I2C interfacing subsystem 
+- USBI2C01A (USB HID interface with Silicon Labs cp2112)
+
 """
 
 
