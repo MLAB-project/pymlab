028a82ff3e7686c8708c94565abef199e46a2cbb
Jakub Kákona <kaklik@mlab.cz>
2014-04-17 18:38:23 +0200

    Jan Milik added support for configuration selection at startup.

--- examples/altimet01_example.py
+++ examples/altimet01_example.py
@@ -5,49 +5,63 @@
 import time
 import datetime
 import sys
+import logging 
+logging.basicConfig(level=logging.DEBUG) 
+
 
 from pymlab import config
 
 
 #### Script Arguments ###############################################
 
-if len(sys.argv) != 2:
+if len(sys.argv) not in (2, 3):
     sys.stderr.write("Invalid number of arguments.\n")
     sys.stderr.write("Usage: %s #I2CPORT\n" % (sys.argv[0], ))
     sys.exit(1)
 
-port    = eval(sys.argv[1])
+port       = eval(sys.argv[1])
+if len(sys.argv) > 2:
+    cfg_number = eval(sys.argv[2])
+else:
+    cfg_number = 0
 
 #### Sensor Configuration ###########################################
 
-cfg = config.Config(
-    i2c = {
-        "port": port,
-    },
-
-    bus = [
-        {
-            "type": "i2chub",
-            "address": 0x72,
-            
-            "children": [
-                {"name": "altimet", "type": "altimet01" , "channel": 7, },   
-            ],
+cfglist=[
+    config.Config(
+        i2c = {
+            "port": port,
         },
-    ],
-)
 
-cfg = config.Config(
-    i2c = {
-        "port": port,
-    },
-    bus = [
-        {
-            "name":          "altimet",
-            "type":        "altimet01",
+        bus = [
+            {
+                "type": "i2chub",
+                "address": 0x72,
+                
+                "children": [
+                    {"name": "altimet", "type": "altimet01" , "channel": 7, },   
+                ],
+            },
+        ],
+    ),
+    config.Config(
+        i2c = {
+            "port": port,
         },
-    ],
-)
+        bus = [
+            {
+                "name":          "altimet",
+                "type":        "altimet01",
+            },
+        ],
+    ),
+]
+
+try:
+    cfg = cfglist[cfg_number]
+except IndexError:
+    sys.stdout.write("Invalid configuration number.")
+    sys.exit(1)
 
 cfg.initialize()
 gauge = cfg.get_device("altimet")
