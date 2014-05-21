b9d9ffdf324e295fef653349a22f863a320ab48e
Jakub Kákona <kaklik@mlab.cz>
2014-05-21 13:02:29 +0200

    example for IMU01A accelerometer sensor was added.

---
+++ examples/imu01_example.py
@@ -0,0 +1,78 @@
+#!/usr/bin/python
+
+# Python example of use pymlab module to read from MLAB's IMU01A module with MMA8451Q  Digital Accelerometer sensor
+
+#uncomment for debbug purposes
+#import logging
+#logging.basicConfig(level=logging.DEBUG) 
+
+
+import time
+import datetime
+import sys
+
+from pymlab import config
+
+
+#### Script Arguments ###############################################
+
+if len(sys.argv) != 2:
+	sys.stderr.write("Invalid number of arguments.\n")
+	sys.stderr.write("Usage: %s #I2CPORT\n" % (sys.argv[0], ))
+	sys.exit(1)
+
+port    = eval(sys.argv[1])
+
+#### Sensor Configuration ###########################################
+'''
+cfg = config.Config(
+    i2c = {
+        "port": port,
+    },
+
+	bus = [
+		{
+            "type": "i2chub",
+            "address": 0x70,
+            
+            "children": [
+                {"name": "acc", "type": "imu01_acc", "sensitivity": 4.0, "channel": 0, }
+            ],
+		},
+	],
+)
+'''
+
+cfg = config.Config(
+    i2c = {
+        "port": port,
+    },
+    bus = [
+        {
+            "name":          "acc",
+            "type":        "imu01_acc",
+            "sensitivity":        2.0,
+        },
+    ],
+)
+
+
+cfg.initialize()
+acc = cfg.get_device("acc")
+sys.stdout.write(" MLAB accelerometer sensor IMU01A module example \r\n")
+time.sleep(0.5)
+
+#### Data Logging ###################################################
+
+sys.stdout.write("Magnetometer data acquisition system started \n")
+
+try:
+    while True:
+        acc.route()
+        (x, y, z) = acc.axes()
+        #sys.stdout.write("\rHeading: " + magnetometer.degrees(magnetometer.heading()) + " X: " + str(x) + " Y: " + str(y) + " Z: " + str(z) + "    " )
+        sys.stdout.write(" X: " + str(x) + " Y: " + str(y) + " Z: " + str(z) + "    " + "\r\n")
+        sys.stdout.flush()
+        time.sleep(0.2)
+except KeyboardInterrupt:
+	sys.exit(0)
