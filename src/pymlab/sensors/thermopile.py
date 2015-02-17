c0f950ee9f03b750958ad44210ccaaa921a834be
kakl <kakl@astro.cz>
2015-02-17 23:29:47 +0100

    Comments

--- src/pymlab/sensors/thermopile.py
+++ src/pymlab/sensors/thermopile.py
@@ -30,15 +30,20 @@ class THERMOPILE01(Device):
     def __init__(self, parent = None, address = 0x5A, **kwargs):
         Device.__init__(self, parent, address, **kwargs)
 
+    # Function for changing communication address if device does not use default address
+    # Warning, the device address can not be changed through CP2112
     def setAddress(self, address):
         self.address = address;
 
+    # Reading temperature of device case in Celsius
     def getTambient(self):
         return self.bus.read_word_data(self.address, 0x06) * 0.02 - 273.15;        
 
+    # Reading of temperature of distant object from zone 1 in Celsius
     def getTobject1(self):
         return self.bus.read_word_data(self.address, 0x07) * 0.02 - 273.15;
 
+    # Reading of temperature of distant object from zone 2 in Celsius
     def getTobject2(self):
         return self.bus.read_word_data(self.address, 0x08) * 0.02 - 273.15;
 
