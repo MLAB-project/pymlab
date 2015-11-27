a58b414e2926c14610df82d1f4fbd26f2ed56595
Roman Dvorak <roman-dvorak@email.cz>
2015-11-27 22:26:48 +0100

    axis class edit, add Run, ACC and DCC functions

--- examples/I2CSPI_HBSTEP.py
+++ examples/I2CSPI_HBSTEP.py
@@ -13,8 +13,9 @@ from pymlab import config
 
 
 class axis:
-    def __init__(self, SPI_CS, Direction, StepsPerUnit):
+    def __init__(self, spi, SPI_CS, Direction, StepsPerUnit):
         ' One axis of robot '
+        self.spi = spi
         self.CS = SPI_CS
         self.Dir = Direction
         self.SPU = StepsPerUnit
@@ -22,27 +23,39 @@ class axis:
 
     def Reset(self):
         ' Reset Axis and set default parameters for H-bridge '
-        spi.SPI_write_byte(self.CS, 0xC0)      # reset
-        spi.SPI_write_byte(self.CS, 0x14)      # Stall Treshold setup
-        spi.SPI_write_byte(self.CS, 0x7F)  
-        spi.SPI_write_byte(self.CS, 0x14)      # Over Current Treshold setup 
-        spi.SPI_write_byte(self.CS, 0x0F)  
-        #spi.SPI_write_byte(self.CS, 0x15)      # Full Step speed 
-        #spi.SPI_write_byte(self.CS, 0x00)
-        #spi.SPI_write_byte(self.CS, 0x30) 
-        #spi.SPI_write_byte(self.CS, 0x0A)      # KVAL_RUN
-        #spi.SPI_write_byte(self.CS, 0x50)
+        self.spi.SPI_write_byte(self.CS, 0xC0)      # reset
+        self.spi.SPI_write_byte(self.CS, 0x14)      # Stall Treshold setup
+        self.spi.SPI_write_byte(self.CS, 0x7F)  
+        self.spi.SPI_write_byte(self.CS, 0x14)      # Over Current Treshold setup 
+        self.spi.SPI_write_byte(self.CS, 0x0F)  
+        #self.spi.SPI_write_byte(self.CS, 0x15)      # Full Step speed 
+        #self.spi.SPI_write_byte(self.CS, 0x00)
+        #self.spi.SPI_write_byte(self.CS, 0x30) 
+        #self.spi.SPI_write_byte(self.CS, 0x0A)      # KVAL_RUN
+        #self.spi.SPI_write_byte(self.CS, 0x50)
       
     def MaxSpeed(self, speed):
         ' Setup of maximum speed '
-        spi.SPI_write_byte(self.CS, 0x07)       # Max Speed setup 
-        spi.SPI_write_byte(self.CS, 0x00)
-        spi.SPI_write_byte(self.CS, speed)  
+        self.spi.SPI_write_byte(self.CS, 0x07)       # Max Speed setup 
+        self.spi.SPI_write_byte(self.CS, 0x00)
+        self.spi.SPI_write_byte(self.CS, speed)  
+      
+    def ACC(self, speed):
+        ' Setup of speed profile acceleration '
+        self.spi.SPI_write_byte(self.CS, 0x05)       # Max Speed setup 
+        self.spi.SPI_write_byte(self.CS, 0x00)
+        self.spi.SPI_write_byte(self.CS, speed)
+    
+    def DCC(self, speed):
+        ' Setup of speed profile deceleration '
+        self.spi.SPI_write_byte(self.CS, 0x06)       # Max Speed setup 
+        self.spi.SPI_write_byte(self.CS, 0x00)
+        self.spi.SPI_write_byte(self.CS, speed)
 
     def ReleaseSW(self):
         ' Go away from Limit Switch '
         while self.ReadStatusBit(2) == 1:           # is Limit Switch ON ?
-            spi.SPI_write_byte(self.CS, 0x92 | (~self.Dir & 1))     # release SW 
+            self.spi.SPI_write_byte(self.CS, 0x92 | (~self.Dir & 1))     # release SW 
             while self.IsBusy():
                 pass
             self.MoveWait(10)           # move 10 units away
@@ -51,9 +64,9 @@ class axis:
         ' Go to Zero position '
         self.ReleaseSW()
 
-        spi.SPI_write_byte(self.CS, 0x82 | (self.Dir & 1))       # Go to Zero
-        spi.SPI_write_byte(self.CS, 0x00)
-        spi.SPI_write_byte(self.CS, speed)  
+        self.spi.SPI_write_byte(self.CS, 0x82 | (self.Dir & 1))       # Go to Zero
+        self.spi.SPI_write_byte(self.CS, 0x00)
+        self.spi.SPI_write_byte(self.CS, speed)  
         while self.IsBusy():
             pass
         time.sleep(0.3)
@@ -63,7 +76,7 @@ class axis:
         ' Move some distance units from current position '
         steps = units * self.SPU  # translate units to steps 
         if steps > 0:                                          # look for direction
-            spi.SPI_write_byte(self.CS, 0x40 | (~self.Dir & 1))       
+            self.spi.SPI_write_byte(self.CS, 0x40 | (~self.Dir & 1))
         else:
             spi.SPI_write_byte(self.CS, 0x40 | (self.Dir & 1)) 
         steps = int(abs(steps))     
@@ -77,17 +90,29 @@ class axis:
         while self.IsBusy():
             pass
 
+    def Run(self, spd):
+        ' The Run command produces a motion at SPD speed '
+        if spd > 0:                                          # look for direction
+            self.spi.SPI_write_byte(self.CS, 0x50 | (self.Dir & 1))
+        else:
+            self.spi.SPI_write_byte(self.CS, 0x51 | (self.Dir & 1))
+            spd = spd *-1
+        print spd  
+        self.spi.SPI_write_byte(self.CS, (spd >> 16) & 0xFF)
+        self.spi.SPI_write_byte(self.CS, (spd >>  8) & 0xFF)
+        self.spi.SPI_write_byte(self.CS, (spd >>  0) & 0xFF)
+
     def Float(self):
         ' switch H-bridge to High impedance state '
-        spi.SPI_write_byte(self.CS, 0xA0)
+        self.spi.SPI_write_byte(self.CS, 0xA0)
 
     def ReadStatusBit(self, bit):
         ' Report given status bit '
-        spi.SPI_write_byte(self.CS, 0x39)   # Read from address 0x19 (STATUS)
-        spi.SPI_write_byte(self.CS, 0x00)
-        data0 = spi.SPI_read_byte()           # 1st byte
-        spi.SPI_write_byte(self.CS, 0x00)
-        data1 = spi.SPI_read_byte()           # 2nd byte
+        self.spi.SPI_write_byte(self.CS, 0x39)   # Read from address 0x19 (STATUS)
+        self.spi.SPI_write_byte(self.CS, 0x00)
+        data0 = self.spi.SPI_read_byte()           # 1st byte
+        self.spi.SPI_write_byte(self.CS, 0x00)
+        data1 = self.spi.SPI_read_byte()           # 2nd byte
         #print hex(data0), hex(data1)
         if bit > 7:                                   # extract requested bit
             OutputBit = (data0 >> (bit - 8)) & 1
@@ -105,8 +130,7 @@ class axis:
 
 # End Class axis --------------------------------------------------
 
-
-
+'''
 cfg = config.Config(
     i2c = {
         "port": 1,
@@ -120,6 +144,16 @@ cfg = config.Config(
                 { "name":"spi", "type":"i2cspi", "channel": 1, },
             ],
         },
+    ],
+)
+'''
+
+cfg = config.Config(
+    i2c = {
+        "port": 1,
+    },
+    bus = [
+        { "name":"spi", "type":"i2cspi"},
     ],
 )
 
@@ -138,7 +172,7 @@ try:
     time.sleep(1)
 
     print "Axis inicialization"
-    X = axis(spi.I2CSPI_SS0, 0, 641)    # set Number of Steps per axis Unit and set Direction of Rotation
+    X = axis(spi, spi.I2CSPI_SS0, 0, 641)    # set Number of Steps per axis Unit and set Direction of Rotation
     X.MaxSpeed(20)                      # set maximal motor speed 
 
     print "Axis is running"
