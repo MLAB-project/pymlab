914dc38caec4524b3a6a4aabb52754cdc1dad166
Honza <chroustjan@ust.cz>
2016-05-22 10:08:47 +0200

    Modifications

--- src/pymlab/config.py
+++ src/pymlab/config.py
@@ -111,6 +111,7 @@ class Config(object):
             "DS4520": gpio.DS4520,
             "i2cspi": bus_translators.I2CSPI,
             "isl01": light.ISL01,
+	    "isl03": light.ISL03,
             "lioncell": lioncell.LIONCELL, #LION1CELL and LION2CELL
             "thermopile01": thermopile.THERMOPILE01,
             "i2cadc01": adc.I2CADC01,
--- src/pymlab/sensors/light.py
+++ src/pymlab/sensors/light.py
@@ -64,6 +64,59 @@ Python library for ISL2902001A MLAB module with ISL29020 I2C Light Sensor
         Ecal = 1 * DATA
         return Ecal
 
+class ISL03(Device):
+    """
+Python library for ISL03A MLAB module with VEML 6030 I2C Light Sensor
+    """
+
+  
+
+    def __init__(self, parent = None, address = 0x10, fault_queue = 1, **kwargs):
+        Device.__init__(self, parent, address, **kwargs)
+
+## register definitions 
+        self.command = 0x00
+        self.Data_lsb = 0x04
+        ##self.Data_msb = 0x02
+
+## config parameters
+        self.SHUTDOWN = (1 << 7)
+        self.continuous_measurement = (1 << 6)
+        self.IR_sense = (1 << 5)
+        self.VIS_sense = (0 << 5)
+        self.clock_INT_16bit = (0b000 << 4)
+        self.clock_INT_12bit = (0b001 << 4)
+        self.clock_INT_8bit = (0b010 << 4)
+        self.clock_INT_4bit = (0b011 << 4)
+        self.clock_EXT_ADC = (0b100 << 4)
+        self.clock_EXT_Timer = (0b101 << 4)
+        self.range_1kLUX = 0b00
+        self.range_4kLUX = 0b01
+        self.range_16kLUX = 0b10
+        self.range_64kLUX = 0b11
+
+    def ADC_sync(self):
+        """ 
+        Ends the current ADC-integration and starts another. Used only with External Timing Mode.
+        """
+        self.bus.write_byte_data(self.address, 0xff, 0x01)
+        LOGGER.debug("syncing ILS ADC",)
+        return
+
+    
+    def config(self, config):
+        self.bus.write_word_data(self.address, self.command, config)
+        return
+
+
+    def get_lux(self):
+        DATA = self.bus.read_word_data(self.address, self.Data_lsb)
+        Ecal = 1 * DATA
+        return Ecal
+
+
+
+
 class RGBC01(Device):
     """
 Python library for RGBC01A MLAB module with TCS3771 I2C Light Sensor
