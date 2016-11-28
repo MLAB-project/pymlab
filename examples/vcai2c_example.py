babab5c6a0157a2855baf0e852f62fa088ba6672
hanuson1 <hanuson1@fel.cvut.cz>
2016-11-28 14:46:38 +0000

    I2C addressing procedure repaired

--- examples/vcai2c_example.py
+++ examples/vcai2c_example.py
@@ -27,8 +27,14 @@ cfg = config.Config(
     },
     bus = [
         {
-            "name":          "current_sensor",
+            "name":          "current_sensor1",
             "type":        "vcai2c01",
+            "address":        0x68,
+        },
+        {
+            "name":          "current_sensor2",
+            "type":        "vcai2c01",
+            "address":        0x6a,
         },
     ],
 )
@@ -36,7 +42,8 @@ cfg.initialize()
 
 print "Current loop sensor example \r\n"
 print "Time, channel #1,  channel #2,  channel #3 ,  channel #4,  channel #5   \r\n"
-sensor = cfg.get_device("current_sensor")
+sensor1 = cfg.get_device("current_sensor1")
+sensor2 = cfg.get_device("current_sensor1")
 time.sleep(0.5)
 
 #### Data Logging ###################################################
@@ -45,25 +52,25 @@ try:
     with open(log_file, "a") as f:
         while True:
 
-            sensor.setADC(address = 0x68, channel = 1, gain = 1, sample_rate = 15);
+            sensor1.setADC(channel = 1, gain = 1, sample_rate = 15);
             time.sleep(0.5)
-            channel1 = sensor.readCurrent();
+            channel1 = sensor1.readCurrent();
 
-            sensor.setADC(address = 0x68, channel = 2, gain = 1, sample_rate = 15);
+            sensor1.setADC(channel = 2, gain = 1, sample_rate = 15);
             time.sleep(0.5)
-            channel2 = sensor.readCurrent();
+            channel2 = sensor1.readCurrent();
 
-            sensor.setADC(address = 0x68, channel = 3, gain = 1, sample_rate = 15);
+            sensor1.setADC(channel = 3, gain = 1, sample_rate = 15);
             time.sleep(0.5)
-            channel3 = sensor.readCurrent();
+            channel3 = sensor1.readCurrent();
 
-            sensor.setADC(address = 0x68, channel = 4, gain = 1, sample_rate = 15);
+            sensor1.setADC(channel = 4, gain = 1, sample_rate = 15);
             time.sleep(0.5)
-            channel4 = sensor.readCurrent();
+            channel4 = sensor1.readCurrent();
 
-            sensor.setADC(address = 0x6A, channel = 1, gain = 1, sample_rate = 15);
+            sensor2.setADC(channel = 1, gain = 1, sample_rate = 15);
             time.sleep(0.5)
-            channel5 = sensor.readCurrent();
+            channel5 = sensor2.readCurrent();
 
             sys.stdout.write("%s \t %0.3f \t %0.3f \t %0.3f \t %0.3f \t %0.3f \n" % (datetime.datetime.now().isoformat(), channel1, channel2, channel3, channel4, channel5))
 
